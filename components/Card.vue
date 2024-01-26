f5b879caf3000a9b8f57afefa7a4b6418eed13ad
Nashib Jr <nashibjr@gmail.com>
2024-01-26 07:24:54 -0800

    Added the Login page

---
+++ components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="card-content">
+    <slot />
+  </div>
+</template>
+
+<style lang="css" scoped>
+.card-content {
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,37 @@
+<script setup>
+const username = ref("");
+const password = ref("");
+</script>
+
 <template>
-  <h1 className="text-red-500">hELLO WORLD</h1>
+  <div class="container flex flex-col justify-center items-center mt-10 pt-10">
+    <h1 class="text-center text-3xl font-bold sm:text-4xl">Blog App</h1>
+    <div class="login-content p-3 sm:p-5 mt-5 sm:mt-14">
+      <h2 class="text-xl font-semibold opacity-90 sm:text-2xl">Sign in</h2>
+      <p class="font-semibold opacity-70 text-sm sm:text-base">
+        Make and view posts
+      </p>
+      <form class="mt-3">
+        <input
+          type="text"
+          v-model="username"
+          placeholder="User name"
+          class="border border-[#f3f3f3] rounded-md mt-3 outline-none p-2 w-full"
+        />
+        <input
+          type="password"
+          v-model="password"
+          placeholder="Password"
+          class="border border-[#f3f3f3] rounded-md mt-3 outline-none p-2 w-full"
+        />
+        <button
+          type="button"
+          class="border border-[#f3f3f3] rounded-md mt-3 bg-[#4875B4] text-white hover:opacity-80 outline-none p-2 w-full"
+          @click="() => console.log(username, password, '>>>>')"
+        >
+          Sign in
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
